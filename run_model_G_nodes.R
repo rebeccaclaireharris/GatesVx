ed21ed5f5de6d47540d43e2ce4b4358887cd18e4
rebeccaclaireharris <rebecca.harris@lshtm.ac.uk>
2017-01-05 09:43:14 +0000

    Secondary outcome of inc and mort rates in 2035 is set up to be output

--- run_model_G_nodes.R
+++ run_model_G_nodes.R
@@ -30,7 +30,7 @@ cntry<-"China"
 
 
 #### Vaccine interventions  ####
-typen<-3 ## Number of vaccine types (PPI, PRI, PSI_LR)
+typen<-1 ## Number of vaccine types (PPI, PRI, PSI_LR)
 effInf<-seq(0,100,10)/100   #efficacy for POI
 effDis<-seq(0,100,10)/100   #efficacy for POD
 durs<-c(2,3,5,7,10,15,20,25,100) #duration of protection (yrs)
@@ -55,6 +55,8 @@ cumulvxyrI<-c()
 NumV<-c()
 inc2050<-c()
 mort2050<-c()
+inc2035<-c()
+mort2035<-c()
 rrun_dfvx<-c()
 vacnames<-c()
 
@@ -82,7 +84,8 @@ if (C==1){kkk<-as.numeric(Sys.getenv("SGE_TASK_ID"))}
   new_mort<-cbind(TBMo,0,0)
   inc2050<-rbind(TBI[151,])
   mort2050<-rbind(TBM[151,])
-  
+  inc2035<-rbind(TBI[136,])
+  mort2035<-rbind(TBM[136,])
   
   eee<-cbind(Xn,0,0,0,0,0); colnames(eee)<-c(colnames(Xn),"type","VE_I","VE_D","dur","count")
   #dfvx<-rbind(dfvx,eee)
@@ -135,6 +138,9 @@ if (C==1){kkk<-as.numeric(Sys.getenv("SGE_TASK_ID"))}
           inc2050<-rbind(inc2050,TBI[151,])
           mort2050<-rbind(mort2050,TBM[151,])
           
+          #outputting 2035 incidence rate
+          inc2035<-rbind(inc2035,TBI[136,])
+          mort2035<-rbind(mort2035,TBM[136,])
           
         }}}}
   #assign('dfvx',dfvx,envir=.GlobalEnv)
@@ -144,18 +150,22 @@ if (C==1){kkk<-as.numeric(Sys.getenv("SGE_TASK_ID"))}
   assign('NumV',NumV,envir=.GlobalEnv)
   assign('inc2050',inc2050,envir=.GlobalEnv)
   assign('mort2050',mort2050,envir=.GlobalEnv)
+  assign('inc2035',inc2035,envir=.GlobalEnv)
+  assign('mort2035',mort2035,envir=.GlobalEnv)
   assign('rrun_dfvx',rrun_dfvx,envir=.GlobalEnv)
   assign('vacnames',vacnames,envir=.GlobalEnv)
   assign('kkk',kkk,envir=.GlobalEnv)
 
 
   
-  setwd("Vaccoutput")
+  setwd("Vxoutput")
   write.table(new_active,paste('new_active_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
   write.table(new_mort,paste('new_mort_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
   write.table(NumV,paste('number_vaccinated_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
   write.table(inc2050,paste('inc_rates_2050_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
   write.table(mort2050,paste('mort_rates_2050_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
+  write.table(inc2035,paste('inc_rates_20350',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
+  write.table(mort2035,paste('mort_rates_2035_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
   write.table(rrun_dfvx,paste('rrun_dfvx_',kkk,'.csv',sep=""),sep=",",row.names=FALSE)
   
   print(count)
